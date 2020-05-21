9cbcf9ac36285e2a8ab147c8d7c876d08ed8e352
ultrasonicsoft <balramchavan@gmail.com>
2020-05-21 17:05:40 +0200

    dashboard layout organized

--- employee-todo-list-app/src/app/app-routing.module.ts
+++ employee-todo-list-app/src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- employee-todo-list-app/src/app/shared/shared-mat.module.ts
+++ employee-todo-list-app/src/app/shared/shared-mat.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
     exports: [
         MatButtonModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatGridListModule
     ]
 })
 export class SharedMatModule { }
